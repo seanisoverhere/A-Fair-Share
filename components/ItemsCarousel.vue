dd935f28ff4953041322ae76359f8450b29b432f
Ambrose Ang <ambrose.tk.ang@gmail.com>
2021-04-05 22:55:50 +0800

    homepage shenanigans

--- components/ItemsCarousel.vue
+++ components/ItemsCarousel.vue
@@ -10,7 +10,7 @@
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{post.item_name}}</h1>
             <p class="leading-relaxed mb-5">{{post.item_desc}}</p>
             <div class="flex items-center flex-wrap ">
-              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Order now
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" @click="handlePostClick(post.item_id)">Order now
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
@@ -40,7 +40,6 @@
 </template>
 
 <script>
-import axios from "axios";
 export default {
   data() {
     return {
@@ -88,6 +87,13 @@ export default {
       this.errors.push(e);
     }
   },
+  methods: {
+    handlePostClick(id) {
+      console.log(id)
+      const url = '/product/' + id
+      this.$router.push(url)
+    }
+  }
 };
 </script>
 <style scoped>
@@ -97,6 +103,6 @@ export default {
   justify-content: center;
   align-items: center;
   width: 500px;
-  height: 520px;
+  height: 550px;
 }
 </style>
